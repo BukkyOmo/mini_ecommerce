bd574eeb6633701e86f7c5d3e60d80fcab5473d8
BukkyOmo <odunayobukky1@gmail.com>
2020-06-20 18:09:22 +0100

    Implement user gets own products

--- src/routes/product.js
+++ src/routes/product.js
@@ -7,5 +7,6 @@ const router = Router();
 
 router.post('/', verifyToken, ProductMiddleware.ValidateCreateProduct, ProductController.createProduct);
 router.get('/', verifyToken, ProductController.getProductsToBuy);
+router.get('/mine', verifyToken, ProductController.getProductsByUser);
 
 export default router;

2e669981d95d71712fe5bed6dc44db8631d6195b
BukkyOmo <odunayobukky1@gmail.com>
2020-06-20 19:18:25 +0100

    Implement user update own product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -152,13 +152,67 @@ class ProductController {
                         status: 'Success',
                         product
                     });
-                }); 
+                });
         } catch (error) {
             return res.status(500).json({
                 message: 'Internal server error',
                 statusCode: 500,
                 status: 'Failure'
-            }); 
+            });
+        }
+    }
+
+    /**
+    * user update product
+    * @param  {object} req - object
+    * @param {object} res - response object
+    * @return {json} res.json
+    */
+    static async updateProduct(req, res) {
+        const { name, price, description, image_url } = req.body;
+        const updated_at = Date.now();
+        const { product_id } = req.params;
+        const { id: created_by } = req.user;
+        try {
+            Product.findById(product_id)
+                .exec((err, product) => {
+                    if (!product) {
+                        return res.status(400).json({
+                            message: 'Product you are trying to update does not exist in database',
+                            statusCode: 400,
+                            status: 'Failure'
+                        });
+                    }
+                    if (created_by != product.created_by) {
+                        return res.status(400).json({
+                            message: 'Sorry, you cannot update this product',
+                            statusCode: 400,
+                            status: 'Failure'
+                        });
+                    }
+                    Product.findOneAndUpdate({ _id: product_id }, { name, price, description, image_url, updated_at }, { new: true })
+                        .exec((err, updatedProduct) => {
+                            if (err) {
+                                return res.status(400).json({
+                                    message: 'Products failed to update',
+                                    statusCode: 400,
+                                    status: 'Failure'
+                                });
+                            }
+                            return res.status(200).json({
+                                message: 'Product updated successfully',
+                                statusCode: 200,
+                                status: 'Success',
+                                updatedProduct
+                            });
+                        });
+                });
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Internal server error',
+                statusCode: 500,
+                status: 'Failure'
+            });
         }
     }
 }

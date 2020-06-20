f3cd1a7b3303ad412742431a1e4538b661b0a8d3
BukkyOmo <odunayobukky1@gmail.com>
2020-06-20 18:29:46 +0100

    Implement user gets a product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,6 +1,12 @@
 import Product from '../models/product';
 
 class ProductController {
+    /**
+    * create a product
+    * @param  {object} req - object
+    * @param {object} res - response object
+    * @return {json} res.json
+    */
     static async createProduct(req, res) {
         const { name, price, description, image_url } = req.body;
         const { id: created_by } = req.user;
@@ -45,6 +51,12 @@ class ProductController {
         }
     }
 
+    /**
+    * get products to buy
+    * @param  {object} req - object
+    * @param {object} res - response object
+    * @return {json} res.json
+    */
     static async getProductsToBuy(req, res) {
         const { id: created_by } = req.user;
         try {
@@ -74,6 +86,12 @@ class ProductController {
         }
     }
 
+    /**
+    * get my products
+    * @param  {object} req - object
+    * @param {object} res - response object
+    * @return {json} res.json
+    */
     static async getProductsByUser(req, res) {
         const { id: created_by } = req.user;
         try {
@@ -100,6 +118,47 @@ class ProductController {
                 statusCode: 500,
                 status: 'Failure'
             });
+        }
+    }
+
+    /**
+    * get one product
+    * @param  {object} req - object
+    * @param {object} res - response object
+    * @return {json} res.json
+    */
+    static async getOneProduct(req, res) {
+        const { product_id } = req.params;
+        try {
+            Product.findById(product_id)
+                .exec((err, product) => {
+                    if (err) {
+                        return res.status(400).json({
+                            message: `Product with id ${product_id} failed to fetch`,
+                            statusCode: 400,
+                            status: 'Failure'
+                        });
+                    }
+                    if (!product) {
+                        return res.status(400).json({
+                            message: 'Product does not exist in database',
+                            statusCode: 400,
+                            status: 'Failure'
+                        });
+                    }
+                    return res.status(200).json({
+                        message: 'Product fetched successfully',
+                        statusCode: 200,
+                        status: 'Success',
+                        product
+                    });
+                }); 
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Internal server error',
+                statusCode: 500,
+                status: 'Failure'
+            }); 
         }
     }
 }
